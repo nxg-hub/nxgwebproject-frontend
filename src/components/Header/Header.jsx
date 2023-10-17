bc4ba9b86049588aa9560ba6b88f3aced76fb402
Olumide Micheal <olumidemicheal7@gmail.com>
2023-10-17 15:46:15 +0100

    add mobile menu

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -15,6 +15,11 @@ import arrow from '../../assets/svgs/arrowNav.svg';
 // Define the Header component
 const Header = () => {
   const [active, setActive] = useState(false);
+  const [open, setOpen] = useState(false);
+
+  const handleMenu = () => {
+    setOpen(!open);
+  }
 
   return (
     <header className='w-screen center'>
@@ -26,7 +31,7 @@ const Header = () => {
               </NavLink>
           </div>
             
-          <div className='nav-list w-full between h-full px-[0.40rem] rounded-[4rem]'>
+          <div className='nav-list w-full between h-full px-[0.40rem] rounded-[4rem] sm:hidden lg:flex'>
             <ul className={`nav-menu h-full between ${active ? 'active center' : ''}`}>
         
               <li className='nav_item h-full center'>
@@ -75,7 +80,7 @@ const Header = () => {
           </div>
         </div>
         
-        <div className='pr-2 pl-6 bg-secondary rounded-[3rem] h-full between'>
+        <div className='pr-2 pl-6 bg-secondary rounded-[3rem] h-full between sm:hidden lg:flex'>
             <NavLink exact to='/contact' className="between gap-3">
               <div style={{color:"white"}}> Contact Us </div>
                 <div className='bg-primary w-[40px] h-[40px] center rounded-full'>
@@ -83,7 +88,55 @@ const Header = () => {
                 </div>
             </NavLink>
         </div>
+
+        <div className='text-secondary text-xl lg:hidden' onClick={handleMenu}>
+            <FaBars />
+        </div>
       </nav>
+      {open && (
+        <div className='absolute w-screen h-[60vh] top-0 bottom-0 px-2 py-6 bg-secondary text-primary flex-col flex'>
+          <div className='flex items-end text-xl justify-end w-full' onClick={handleMenu}>
+            <FaTimes />
+          </div>
+
+          <div className='flex items-start mt-8 w-full'>
+            <ul className={`flex-col gap-4 h-full flex w-full`}>
+          
+              <li className='w-full'>
+                <NavLink exact to='/'  className="nav-links">
+                  Home
+                </NavLink>
+              </li>
+
+              <li className='w-full'>
+                <NavLink exact to='/about' className="nav-links">
+                  About
+                </NavLink>
+              </li>
+
+              <li className='w-full'>
+                <NavLink exact to='/services'  className="nav-links">
+                  Services
+                </NavLink>
+              </li>
+
+              <li className='w-full'>
+                <NavLink exact to='/career'  className="nav-links">
+                  Career
+                </NavLink>
+              </li>
+
+              <li className='w-full'>
+                <NavLink exact to='/contact' className="nav-links">
+                  Contact                   
+                </NavLink>
+              </li>
+
+            </ul>
+
+          </div>
+        </div>
+      )}
     </header>
   );
 };
