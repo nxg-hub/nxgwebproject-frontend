16c0ef5db747cef9d46e5f7c4410eb80e9d9dc4b
Olumide Micheal <olumidemicheal7@gmail.com>
2023-10-25 12:34:04 +0100

    responsiveness

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -36,33 +36,33 @@ const About = () => {
           </div>
 
           {/* We Bring You Creativity At Its Best */}
-          <div className="between w-[90%]">
-            <div className="flex-col gap-10 items-start flex w-[60%]">
+          <div className="between flex-col md:flex-row w-[90%]">
+            <div className="flex-col gap-10 items-start flex md:w-[60%] mb-[3rem] md:mb-0">
               <h3 className="text-[32px] font-semibold text-secondary">
                 We bring you Creativity at its best!
               </h3>
               <p>NextGenHub (NXG-Hub) is a group of software developers and IT consultants from all spheres of the information technology world, endowed with experiences, bright minds, and exposure. We are a group of innovative minds engaged in constant brainstorming for exceptional ideas. Our collective skills result in a dynamic force that can fulfill all the requirements of your brand or career, no matter how diverse they may be. We are on a mission to re-engineer the world through technology.</p>
             </div>
 
-            <div className="w-[35%]">
+            <div className="md:w-[35%]">
               <img src={AboutOne} alt="About" />
             </div>
           </div>
         </div>
 
         {/* OUR MISSION */}
-<div className="between w-[90%]">
-  <div className="w-[35%] float-left">
-    <img src={AboutTwo} alt="About" />
-  </div>
+        <div className="between w-[90%]">
+          <div className="w-[35%] float-left">
+            <img src={AboutTwo} alt="About" />
+          </div>
 
-  <div className="flex-col gap-10 items-start flex w-[60%]">
-    <h3 className="text-[32px] font-semibold text-secondary">
-      Our Mission
-    </h3>
-    <p>NXG-HUB Digital Technologies Aims To Empower Numerous Youth By Offering Industry-Relevant Skill Training, Thus Enhancing Their Prospects For A More Promising Livelihood. Simultaneously, We Are Dedicated To Delivering Real-World Technological Solutions To Both Corporate And Government Entities, Contributing To The Digitization Of Africa's Economy.</p>
-  </div>
-</div>
+          <div className="flex-col gap-10 items-start flex w-[60%]">
+            <h3 className="text-[32px] font-semibold text-secondary">
+              Our Mission
+            </h3>
+            <p>NXG-HUB Digital Technologies Aims To Empower Numerous Youth By Offering Industry-Relevant Skill Training, Thus Enhancing Their Prospects For A More Promising Livelihood. Simultaneously, We Are Dedicated To Delivering Real-World Technological Solutions To Both Corporate And Government Entities, Contributing To The Digitization Of Africa's Economy.</p>
+          </div>
+        </div>
 
 
         {/* WHAT WE DO */}
