eda085e1cf6c68cb136176d0d1c6262182ac61ee
Adeleyedamilola <temitopeadeleye2016@gmail.com>
2023-11-05 20:03:07 +0100

    Made Final Corrections

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -6,6 +6,7 @@ import Footer from '../../components/Footer';
 import AboutOne from '../../assets/images/aboutOne.jpg';
 import AboutTwo from '../../assets/images/aboutTwo.jpg';
 import AboutThree from '../../assets/images/aboutThree.jpg';
+import LearnArrow from '../../assets/svgs/learnArrow.svg';
 
 const About = () => {
   return (
@@ -37,50 +38,57 @@ const About = () => {
 
         {/* We Bring You Creativity At Its Best */}
       <div className="between flex-col md:flex-row w-[90%]">
-      <div className="order-last md:order-first md:w-[60%] flex-col gap-10 items-start flex mb-[3rem] md:mb-0">
+      <div className="order-last md:order-first md:w-[60%] flex-col gap-4 items-start flex mb-[0rem] md:mb-0">
       <h3 className="lg:text-[32px] md:text-[20px] font-semibold text-secondary">
-      We bring you Creativity at its best!
+      We bring you Creativity at Its best!
       </h3>
-        <p className="lg:text-[18px] md:text-[16px]">
+    <p className="lg:text-[18px] md:text-[16px] text-color">
       NextGenHub (NXG-Hub) is a group of software developers and IT consultants from all spheres of the information technology world, endowed with experiences, bright minds, and exposure. We are a group of innovative minds engaged in constant brainstorming for exceptional ideas. Our collective skills result in a dynamic force that can fulfill all the requirements of your brand or career, no matter how diverse they may be. We are on a mission to re-engineer the world through technology.
-    </p>
+</p>
   </div>
   <div className="order-first md:order-last md:w-[35%]">
-    <img src={AboutOne} alt="About" className="mb-8"/>
+    <img src={AboutOne} alt="About" className="mb-4"/>
   </div>
 </div>
 </div>
+
 
         {/* OUR MISSION */}
         <div className="between flex-col md:flex-row w-[90%]">
   <div className="md:w-[35%] float-left">
-    <img src={AboutTwo} alt="About" className="mb-8" />
+    <img src={AboutTwo} alt="About" className="mb-4" />
   </div>
-  <div className="md:w-[60%] flex-col gap-10 items-start flex md:ml-[20px]">
+  <div className="md:w-[60%] flex-col gap-4 items-start flex gap-4 items-start flex mb-[3rem] md:mb-0">
     <h3 className="lg:text-[32px] md:text-[20px] font-semibold text-secondary">
       Our Mission
     </h3>
-    <p>
+    <p className="lg:text-[18px] md:text-[16px] text-color"> 
       NXG-HUB Digital Technologies Aims To Empower Numerous Youth By Offering Industry-Relevant Skill Training, Thus Enhancing Their Prospects For A More Promising Livelihood. Simultaneously, We Are Dedicated To Delivering Real-World Technological Solutions To Both Corporate And Government Entities, Contributing To The Digitization Of Africa's Economy.
     </p>
   </div>
 </div>
-<div className="mb-20"></div>
+
 
         {/* WHAT WE DO */}
         <div className="between flex-col md:flex-row w-[90%]">
-    <div className="order-last md:order-first md:w-[60%] flex-col gap-10 items-start flex mb-[3rem] md:mb-0">
+    <div className="order-last md:order-first md:w-[60%] flex-col gap-4 items-start flex mb-[0rem] md:mb-0">
     <h3 className="lg:text-[32px] md:text-[20px] font-semibold text-secondary">
       What We Do
     </h3>
-    <p className="lg:text-[18px] md:text-[16px]">
+    <p className="lg:text-[18px] md:text-[16px] text-color">
       We Assist You In Constructing Top-Tier Digital Solutions And Products Following Best Practices, While Also Offering An Extensive Array Of Associated Professional Services. Our Commitment Is To Provide Unparalleled Service To Our Esteemed Clients On A Global Scale.
     </p>
   </div>
-
   <div className="order-first md:order-last md:w-[35%]">
-    <img src={AboutThree} alt="About" className="mb-8" />
+    <img src={AboutThree} alt="About" className="mb-4" />
   </div>
+</div>
+
+<div className="lg:text-left md:text-center my-4">
+    <CustomButton backgroundColor="#FFF" border="1px solid #717171" textColor="#717171" borderRadius="20px" padding="5px 40px">
+        <p>Learn</p>
+            <img src={LearnArrow} alt="learn arrow" />
+    </CustomButton>
 </div>
 
 
