f875158b98569649d0f2a55abcaf595c897a5a45
bolaji1243 <emmanuelbolaji992@gmail.com>
2026-05-01 08:52:13 +0100

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import About from "./pages/About";
 import Services from "./pages/Services";
 import Career from "./pages/Career";
 import Training from "./pages/Training";
+import JobShadowing from  "./pages/JobShadowing";
+import EbooksPage from  "./pages/JobShadowing/ebooks";
 import SuccessfulApplication from "./pages/Form/components/SuccessfulApplication";
 import RegisterForm from "./pages/Form";
 import Login from "./Admin/Login/Index";
@@ -30,6 +32,8 @@ function App() {
           <Route path="/services" element={<Services />} />
           <Route path="/career" element={<Career />} />
           <Route path="/training" element={<Training />} />
+          <Route path="/jobshadowing" element={<JobShadowing />} />
+          <Route path="/jobshadowing/ebooks" element={<EbooksPage />} />
           <Route path="/form" element={<RegisterForm />} />
           <Route path="/scholarship" element={<ScholarshipForm />} />
           <Route path="/ignite" element={<Ignite/>}/>
