bf8f380119fa15d5f7587e23c3caa3ac1283844e
bolaji1243 <emmanuelbolaji992@gmail.com>
2026-05-02 07:55:15 +0100

    changes on the project

---
+++ public/jobshadowing/logos/nxgwebproject-frontend-job-shadowing-changes (1)/nxgwebproject-frontend-job-shadowing-changes/src/Admin/Dashboard/routes/Scholarship/components/ScholarshipCard.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const ScholarshipCard = ({ user }) => {
+  return (
+    <div className="w-[80%] m-auto  h-[300px] border-2 border-[#fff] shadow-lg shadow-[#a0a0a0] rounded-lg ">
+      <img
+        className=" h-[100px] w-[100px] md:h-[120px] md:w-[120px] m-auto mt-3 rounded-full border "
+        src={user.passportUrl}
+        alt="passport"
+      />
+      <h2 className="text-center font-bold mt-3 md:text-md">
+        {user.firstName}
+      </h2>
+      <div className="w-[80%] m-auto text-center bg-secondary py-2 rounded-md text-primary font-bold mt-11">
+        <Link to={`/admin/scholarshipuser/${user.id}`}>View Details</Link>
+      </div>
+    </div>
+  );
+};
+
+export default ScholarshipCard;
